f80e1a46e5631f4931b123047e85901f940881eb
asing44 <109570966+asing44@users.noreply.github.com>
2023-03-14 11:33:54 -0400

    started into

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// console.log(screen.width, screen.height);
+
+// const introAnimation = document.getElementById("-introAnimation");
+
+// introAnimation.style.width = screen.width + "px";
+// introAnimation.style.height = screen.height + "px";
