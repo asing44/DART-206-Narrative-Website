41ba9669d3f4d79434bd6efd9e64898d68a7e66d
asing44 <109570966+asing44@users.noreply.github.com>
2023-03-16 18:42:56 -0400

    Pagination 2.0

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-
-
 var _titleAnimationData = {
     container: document.getElementById("-titleAnimation"),
     renderer: "svg",
@@ -26,31 +24,37 @@ var _paginationAnimation = bodymovin.loadAnimation({
 
 var _titleAnimation = bodymovin.loadAnimation(_titleAnimationData);
 
+window.onload = (event) => {
+    window.scrollTo(0, 0);
+}
+
 _introAnimationData.addEventListener("complete", () => {
+
+    document.body.classList.remove("_open");
 
     setInterval(() => {
         _titleAnimation.play();
-        _paginationAnimation.play();
     }, 500);
 });
+
+function mapNumbers(number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+const _paginationFrame1 = document.getElementsByClassName("_paginationFrame1")[0];
+const _paginationFrame2 = document.getElementsByClassName("_paginationFrame2")[0];
 
 window.onscroll = function() {
     var _scrollPosition = document.documentElement.scrollTop;
     var _windowHeight = window.innerHeight;
 
-    var _paginationAnimationDuration = _paginationAnimation.getDuration(true);
-    var _startFrame = Math.round((_scrollPosition / _windowHeight) * _paginationAnimationDuration);
-    var _endFrame = Math.round(((_scrollPosition + _windowHeight) / _windowHeight) * _paginationAnimationDuration);
-
-    _paginationAnimation.playSegments([_startFrame, _endFrame], true);
+    _paginationAnimation.goToAndStop(mapNumbers(_scrollPosition, 0, _windowHeight, 0, 330), true)
 };
 
-// function _animatePagination() {
-//     var _scrollPosition = window.pageYOffset;
-//     _paginationAnimation.goToAndStop(_scrollPosition, true);
-// }
+// _paginationAnimation.addEventListener("enterFrame", (e) => {
+//     if (e.frame == 59) {
+//         _paginationFrame1.style.stroke = "#29363B";
+//         _paginationFrame2.styke.stroke = "#29363B";
+//     }
 
-setInterval(function() {
-    var currentFrame = _paginationAnimation.currentFrame;
-    console.log("Current frame: " + currentFrame)
-}, 500);
+// })
